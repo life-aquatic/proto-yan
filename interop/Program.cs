e82fd2c3177af404a5878a0fe7fceb5e10770003
life-aquatic <f.a.nikitin@gmail.com>
2021-08-24 19:56:11 +0300

    more interop

---
+++ interop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace interop
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct SystemTime
+    {
+        public ushort Year;
+        public ushort Month;
+        public ushort DayOfWeek;
+        public ushort Day;
+        public ushort Hour;
+        public ushort Minute;
+        public ushort Second;
+        public ushort Milliseconds;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemTime t = new SystemTime();
+            pinvok.GetSystemTime(out t);
+            Console.WriteLine(t.Year);
+
+            //pinvok.MessageBox(IntPtr.Zero, "Please do not press this again.", "Attention", 0);
+
+
+
+            unsafe
+            {
+                EnumWindows(&PrintWindow, IntPtr.Zero);
+                [DllImport("user32.dll")]
+                static extern int EnumWindows(
+                delegate*<IntPtr, IntPtr, bool> hWnd, IntPtr lParam);
+                static bool PrintWindow(IntPtr hWnd, IntPtr lParam)
+                {
+                    Console.WriteLine(hWnd.ToInt64());
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ interop/pinvok.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace interop
+{
+    static class pinvok
+    {
+        
+       
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, int type);
+
+        [DllImport("kernel32.dll")]
+        public static extern void GetSystemTime(out SystemTime t);
+
+    }
+}
+
+
+
